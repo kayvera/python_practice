f076c8e4b2776eb25cea19944042b33430c42c84
kayvera <mikaylarvr@gmail.com>
2021-05-16 17:52:37 -0700

    recursive info

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -4,4 +4,26 @@ def openRussianDoll(doll):
   if doll == 1:
     print("All dolls are opened")
   else: 
-    openRussianDoll(doll-1)
+    openRussianDoll(doll-1) 
+
+# when to choose recursion
+
+"""
+
+if you can define the problem into similar sub problems
+design an algo to compute nth
+write code to list the n
+implement a method to compute all
+
+"""
+
+# common in trees and graphs
+
+# used in divide and conquer, greedy, and DP
+
+def recursiveMethod(n):
+  if n < 1:
+    print("n is less than 1")
+  else: 
+    recursiveMethod(n-1)
+    print(n)

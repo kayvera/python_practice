43896f172f70b42dd8dfa96fb465c670823a7ce5
kayvera <mikaylarvr@gmail.com>
2021-05-18 15:57:30 -0700

    recursive continued

---
+++ recursion/findMax.py
@@ -0,0 +1,9 @@
+def findMaxNum(arr, n):  # M(n)
+    if n == 1:  # O(1)
+        return arr[0]  # O(1)
+    return max(arr[n-1], findMaxNum(arr, n-1))  # M(n-1)
+
+# big-O of recursive algorithm?
+# recursive is assumed to take M(n)
+# equation above equals M(n)=O(1)+M(n-1)
+# equals O(n)

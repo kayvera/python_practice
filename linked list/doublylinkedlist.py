27dc603056e5cc8c42da1c8f146a12dcda10eeb4
kayvera <mikaylarvr@gmail.com>
2021-05-24 17:00:20 -0700

    doubly linked list practice

--- linked list/doublylinkedlist.py
+++ linked list/doublylinkedlist.py
@@ -16,16 +16,77 @@ class DoublyLinkedList:
             yield node
             node = node.next
 
+    #  Creation of Doubly Linked List
     def createDLL(self, nodeValue):
         node = Node(nodeValue)
         node.prev = None
         node.next = None
         self.head = node
         self.tail = node
-        return "The DLL is created successfully"
+        return "The DLL is created Successfully"
+
+    def insertNodeDLL(self, nodeValue, location):
+        if self.head is None:
+            print("The node cannot be inserted")
+        else:
+            newNode = Node(nodeValue)
+            if location == 0:
+                newNode.prev = None
+                newNode.next = self.head
+                self.head.prev = newNode
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                newNode.prev = self.tail
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+
+    def traverseDLL(self):
+        if self.head is None:
+            print("there are no elements to traverse")
+        else:
+            tempNode = self.head
+            while tempNode:
+                print(tempNode.value)
+                tempNode = tempNode.next
+
+    def reverseTraversalDLL(self):
+        if self.head is None:
+            print("There is not any elements to traverse")
+        else:
+            tempNode = self.tail
+            while tempNode:
+                print(tempNode.value)
+                tempNode = tempNode.prev
+
+    def searchDLL(self, nodeValue):
+        if self.head is None:
+            return "There is no elements in the list"
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == nodeValue:
+                    return tempNode.value
+                tempNode = tempNode.next
+            return "the node doesn't exist in the list"
 
 
 doublyLL = DoublyLinkedList()
 doublyLL.createDLL(5)
-
+doublyLL.insertNodeDLL(0, 0)
+doublyLL.insertNodeDLL(2, 1)
+doublyLL.insertNodeDLL(6, 2)
 print([node.value for node in doublyLL])
+# doublyLL.traverseDLL()
+print(doublyLL.searchDLL(6))
+# doublyLL.reverseTraversalDLL()

6a9df0e4b01d061bc27248462f60c49e71271bd5
Mikayla Rivera <mikaylarvr@gmail.com>
2021-07-29 13:20:57 -0700

    remove duplicates alternate solution

--- array/removeDupSort.py
+++ array/removeDupSort.py
@@ -8,3 +8,19 @@ def removeDuplicates(nums):
             slow += 1
             nums[slow] = nums[fast]
     return slow + 1
+
+def remove_duplicates(arr):
+# index of the next non-duplicate element
+    next_non_duplicate = 1
+
+    i = 1
+    while(i < len(arr)):
+        if arr[next_non_duplicate - 1] != arr[i]:
+            arr[next_non_duplicate] = arr[i]
+            next_non_duplicate += 1
+            i += 1
+
+    return next_non_duplicate
+
+
+
